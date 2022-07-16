1ba15ce1d2d351907c9846b27c461ab64171efca
EC2 Default User <ec2-user@ip-172-31-91-39.ec2.internal>
2022-07-16 18:42:11 +0000

    Created session 5

---
+++ session-5/variables.tf
@@ -0,0 +1,23 @@
+variable "instance_type" {
+  type        = string
+  description = "This is an Instance type for EC2"
+  default     = "t2.micro"
+}
+
+variable "key_name" {
+  type        = string
+  description = "This is a Key Pair for EC2"
+  default     = "MyMacKey"
+}
+
+variable "env" {
+  type        = string
+  description = "This is a tag for EC2"
+  default     = "dev"
+}
+
+variable "ingress_ports" {
+  type        = list(string)
+  description = "This is a list of ports for Ingress Rule"
+  default     = ["22", "80", "443", "3306", "11111"]
+}
