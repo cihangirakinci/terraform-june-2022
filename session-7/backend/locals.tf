871aedf9bda28c16652ffae5793e221b5906c1bd
EC2 Default User <ec2-user@ip-172-31-91-39.ec2.internal>
2022-07-21 02:18:41 +0000

    Added session 7

---
+++ session-7/backend/locals.tf
@@ -0,0 +1,10 @@
+# locals {
+#   common_tags = {
+#     environment = var.environment
+#     project = var.project
+#     team = var.team
+#     owner = var.owner
+#     managed_by = var.managed_by
+#     organization = var.organization
+#   }
+# }
