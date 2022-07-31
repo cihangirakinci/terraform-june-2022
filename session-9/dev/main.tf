8cfd15340e8fa6f42f200ed8a3e0ef1208614505
EC2 Default User <ec2-user@ip-172-31-91-39.ec2.internal>
2022-07-31 23:14:41 +0000

    Added session 9

---
+++ session-9/dev/main.tf
@@ -0,0 +1,6 @@
+module "ec2" {
+  source        = "../../modules/ec2"
+  ami           = "ami-0cff7528ff583bf9a"
+  instance_type = "t2.micro"
+  env           = "dev"
+}
---
+++ session-9/qa/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+    bucket         = "terraform-cihangir-session-remote-backend-bucket"
+    key            = "session-9/qa/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-june-2022-state-lock-table"
+  }
+}
